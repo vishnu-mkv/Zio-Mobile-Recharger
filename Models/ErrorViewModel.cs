882716ba1f9e41dcf1fdd7b69e926d8c6f81fb68
Vishnu <mkv1722@gmail.com>
2023-04-25 09:35:30 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mobile_recharger.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
